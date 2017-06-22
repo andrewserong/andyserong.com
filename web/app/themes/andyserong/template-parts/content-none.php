a339e83fa18e0199dd60d4078618b5c13b5f9429
Andrew Serong <andrewserong@gmail.com>
2017-06-22 21:36:43 +1000

    Tweak search form input field
    Remove line-height to fix vertical alignment of placeholder issue on iOS,
    also remove the rendering of the searchbox on the content-none template partial
    since there's usually a search box available on the sidebar anyway.

--- web/app/themes/andyserong/template-parts/content-none.php
+++ web/app/themes/andyserong/template-parts/content-none.php
@@ -37,13 +37,13 @@
 
 			<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'andyserong' ); ?></p>
 			<?php
-				get_search_form();
+				// get_search_form();
 
 		else : ?>
 
 			<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'andyserong' ); ?></p>
 			<?php
-				get_search_form();
+				// get_search_form();
 
 		endif; ?>
 	</div><!-- .page-content -->
