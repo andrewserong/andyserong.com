bac74add1b7b8a639e07818bbadc73aa02db3aab
Andrew Serong <andrewserong@gmail.com>
2017-08-27 18:27:41 +1000

    Update footer, font sizing, and responsive images in entry content

--- web/app/themes/andyserong/footer.php
+++ web/app/themes/andyserong/footer.php
@@ -15,15 +15,29 @@
 
 	<footer id="colophon" class="site-footer site-footer-trees" style="background-image: url('<?php echo get_bloginfo('template_url') ?>/img/trees-subdued.jpg');" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'andyserong' ) ); ?>"><?php
+			<p class="social-media-links">
+				<a href="https://twitter.com/andrewserong/" target="_blank">
+					<i class="fa fa-twitter" aria-hidden="true"></i>
+				</a>
+				<a href="https://instagram.com/andrewserong/" target="_blank">
+					<i class="fa fa-instagram" aria-hidden="true"></i>
+				</a>
+				<a href="https://github.com/andrewserong/" target="_blank">
+					<i class="fa fa-github" aria-hidden="true"></i>
+				</a>
+				<a href="https://codepen.io/andrewserong/" target="_blank">
+					<i class="fa fa-codepen" aria-hidden="true"></i>
+				</a>
+			</p>
+			<p class="barely-visible-footer">
+				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'andyserong' ) ); ?>"><?php
 				/* translators: %s: CMS name, i.e. WordPress. */
 				printf( esc_html__( 'Somewhat warmly powered by %s', 'andyserong' ), 'WordPress' );
-			?></a>
-			<span class="sep"> | </span>
-			<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'andyserong' ), 'andyserong', '<a href="https://twitter.com/andrewserong">Andrew Serong</a>' );
-			?>
+				?>
+				</a>
+				<br/>
+				A <a href="https://github.com/andrewserong/andyserong.com">hand-made theme</a> built on top of <a href="http://underscores.me/" target="_blank">underscores</a>
+			</p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
--- web/app/themes/andyserong/header.php
+++ web/app/themes/andyserong/header.php
@@ -16,6 +16,9 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 
+<!-- add Font Awesome via CDN -->
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 <?php wp_head(); ?>
 </head>
 
